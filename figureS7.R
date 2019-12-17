583f4de88a7f07ca4098aec2cf196c67661e7553
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2019-12-17 16:40:43 +0100

    Better y axis for the raw data plot.

--- figureS7.R
+++ figureS7.R
@@ -24,7 +24,7 @@ plot.data.recenter(
 par(mar=c(5, 4.2, 1, 1))
 plot.data.recenter(
 	recenter(tovar, G=tovar.mat$G, Gv=tovar.mat$Gv, P=tovar.mat$P, N=64, Np=12, target="mu.y", normalization="raw"),
-	main="", ylab=expression("log UBA (mm"^2*")"), xlab="Generation", ylim=c(5.6,6.4))
+	main="", ylab=expression("log UBA (mm"^2*")"), xlab="Generation", ylim=c(5.6,6.3))
 par(mar=c(3, 4.2, 3, 1))
 plot.data.recenter(
 	recenter(tulum, G=tulum.mat$G, Gv=tulum.mat$Gv, P=tulum.mat$P, N=64, Np=12, target="mu.x", normalization="raw"),
@@ -32,5 +32,5 @@ plot.data.recenter(
 par(mar=c(5, 4.2, 1, 1))
 plot.data.recenter(
 	recenter(tulum, G=tulum.mat$G, Gv=tulum.mat$Gv, P=tulum.mat$P, N=64, Np=12, target="mu.y", normalization="raw"),
-	main="", ylab="", xlab="Generation", ylim=c(5.9,6.4))
+	main="", ylab="", xlab="Generation", ylim=c(5.9,6.3))
 dev.off()
