c8f186d544e394462927bc2cbdc726e5162512fc
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-03-25 11:40:53 +0100

    Adding the possibility to plot the data without the prediction (figS8b).

--- figureS8.R
+++ figureS8.R
@@ -34,3 +34,23 @@ plot.data.recenter(
 	recenter(tulum, G=tulum.mat$G, Gv=tulum.mat$Gv, P=tulum.mat$P, N=64, Np=12, target="mu.y", normalization="raw"),
 	main="", ylab="", xlab="Generation", ylim=c(5.9,6.3), CI.factor=CI.factor)
 dev.off()
+
+pdf("figureS8b.pdf", width=8, height=8, useDingbats = FALSE)
+layout(cbind(1:2,3:4))
+par(mar=c(3, 4.2, 3, 1))
+plot.data.recenter(
+	recenter(tovar, G=tovar.mat$G, Gv=tovar.mat$Gv, P=tovar.mat$P, N=64, Np=12, target="mu.x", normalization="raw"),
+	main="Tovar", ylab=expression("log GA (mm"^2*")"), xlab="", ylim=c(2.4,3.4), CI.factor=CI.factor, prediction=FALSE)
+par(mar=c(5, 4.2, 1, 1))
+plot.data.recenter(
+	recenter(tovar, G=tovar.mat$G, Gv=tovar.mat$Gv, P=tovar.mat$P, N=64, Np=12, target="mu.y", normalization="raw"),
+	main="", ylab=expression("log UBA (mm"^2*")"), xlab="Generation", ylim=c(5.6,6.3), CI.factor=CI.factor, prediction=FALSE)
+par(mar=c(3, 4.2, 3, 1))
+plot.data.recenter(
+	recenter(tulum, G=tulum.mat$G, Gv=tulum.mat$Gv, P=tulum.mat$P, N=64, Np=12, target="mu.x", normalization="raw"),
+	main="Tulum", ylab="", xlab="", ylim=c(2.9,3.7), prediction=FALSE)
+par(mar=c(5, 4.2, 1, 1))
+plot.data.recenter(
+	recenter(tulum, G=tulum.mat$G, Gv=tulum.mat$Gv, P=tulum.mat$P, N=64, Np=12, target="mu.y", normalization="raw"),
+	main="", ylab="", xlab="Generation", ylim=c(5.9,6.3), CI.factor=CI.factor, prediction=FALSE)
+dev.off()
